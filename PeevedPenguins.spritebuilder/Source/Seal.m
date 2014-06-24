902d66e836b59e9dc3918082d9450df6d992d18b
DemiPixel <luke5227@gmail.com>
2014-06-24 12:31:45 -0700

    Automatically Committed for MakeGamesWithUs

---
+++ PeevedPenguins.spritebuilder/Source/Seal.m
@@ -0,0 +1,23 @@
+//
+//  Seal.m
+//  PeevedPenguins
+//
+//  Created by Lucas Simon on 6/24/14.
+//  Copyright (c) 2014 Apportable. All rights reserved.
+//
+
+#import "Seal.h"
+
+@implementation Seal
+
+- (id)init {
+    self = [super init];
+    
+    if (self) {
+        CCLog(@"Seal Created!");
+    }
+    
+    return self;
+}
+
+@end
