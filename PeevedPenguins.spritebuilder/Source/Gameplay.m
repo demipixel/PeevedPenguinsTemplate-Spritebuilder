97d8ef0cdb159a59df41911e89a5b16f2a00d88f
DemiPixel <luke5227@gmail.com>
2014-06-25 09:40:31 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -20,6 +20,7 @@
     self.userInteractionEnabled = TRUE;
     CCScene *level = [CCBReader loadAsScene:@"Levels/Level1"];
     [_levelNode addChild:level];
+    _physicsNode.debugDraw = TRUE;
     _pullbackNode.physicsBody.collisionMask = @[];
 }
 
