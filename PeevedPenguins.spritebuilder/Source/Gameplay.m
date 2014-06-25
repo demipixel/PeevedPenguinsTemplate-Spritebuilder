b45a56abe2f2a31a3507d7a4a5c344c072a5f102
DemiPixel <luke5227@gmail.com>
2014-06-25 10:21:52 -0700

    Automatically Committed for MakeGamesWithUs

--- PeevedPenguins.spritebuilder/Source/Gameplay.m
+++ PeevedPenguins.spritebuilder/Source/Gameplay.m
@@ -36,8 +36,8 @@
     
     if (CGRectContainsPoint([_catapultArm boundingBox], touchLocation))
     {
-        offsetTouchX = _mouseJointNode.position.x - touchLocation.x;
-        offsetTouchY = _mouseJointNode.position.y - touchLocation.y;
+        offsetTouchX = touchLocation.x - _mouseJointNode.position.x;
+        offsetTouchY = touchLocation.y - _mouseJointNode.position.y;
         
         _mouseJoint = [CCPhysicsJoint connectedSpringJointWithBodyA:_mouseJointNode.physicsBody bodyB:_catapultArm.physicsBody anchorA:ccp(0, 0) anchorB:ccp(34, 138) restLength:0.f stiffness:3000.f damping:150.f];
     }
